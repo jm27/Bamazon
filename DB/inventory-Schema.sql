56b53d76fe24407dad906f9689ae1bdaae2295b3
Jesus Esquer <jme2791@gmail.com>
2019-11-01 15:52:28 -0700

    added id to schema

--- DB/inventory-Schema.sql
+++ DB/inventory-Schema.sql
@@ -6,13 +6,16 @@ USE bamazon;
 -- Then create a Table inside of that database called `products`
 CREATE TABLE products (
 --    * item_id (unique id for each product)
-
+	id int NOT NULL AUTO_INCREMENT,
 --    * product_name (Name of product)
     product_name VARCHAR(100) NOT NULL,
 --    * department_name
     department_name VARCHAR(100) NOT NULL,
 --    * price (cost to customer)
-    price DECIMAL(10,4) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
 --    * stock_quantity (how much of the product is available in stores)
-    stock_quantity DECIMAL(10,4) NOT NULL,
+    stock_quantity DECIMAL(10,2) NOT NULL,
+
+    PRIMARY KEY (id)
+
 )
